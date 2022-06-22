677791b392f2d220b4af03abf032e9c9ed8db1e2
joaofelix27 <joaofelix27@gmail.com>
2022-06-21 23:04:11 -0300

    Versão com get /participants pegando os participantes do banco de dados

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 const mongoClient = new MongoClient("mongodb://localhost:27017");
@@ -14,17 +15,25 @@ mongoClient.connect().then(() => {
 
 app.post( '/participants' ,(req,res) => {
     let {name} = req.body
+    console.log(name)
     if ( typeof name ==="string" && name) {
         db.collection("users").insertOne({
             name: name,
             LastStatus: Date.now()
         });
-        res.status("201")
+        res.sendStatus(201)
     } else {
-        res.status("402")
+        res.sendStatus("402")
     }
 
 })
+
+app.get("/participants", (req, res) => {
+	// buscando usuários
+	db.collection("users").find().toArray().then(users => {
+		res.send(users); // array de usuários
+	});
+});
 
 
 app.listen(5000);
