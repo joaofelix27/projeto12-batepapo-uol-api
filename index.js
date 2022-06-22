17d32cb79921dfff7fa8e9786009ad0e1fd0e989
joaofelix27 <joaofelix27@gmail.com>
2022-06-21 23:54:37 -0300

    Validando se já existe no banco de dados

--- index.js
+++ index.js
@@ -10,30 +10,42 @@ const mongoClient = new MongoClient("mongodb://localhost:27017");
 let db;
 
 mongoClient.connect().then(() => {
-	db = mongoClient.db("banco_de_dados_uol");
+  db = mongoClient.db("banco_de_dados_uol");
 });
 
-app.post( '/participants' ,(req,res) => {
-    let {name} = req.body
-    console.log(name)
-    if ( typeof name ==="string" && name) {
-        db.collection("users").insertOne({
+app.post("/participants", (req, res) => {
+  let { name } = req.body;
+  db.collection("users")
+    .find({
+      name: name,
+    })
+    .toArray()
+    .then((alreadyExists) => {
+      console.log(alreadyExists); // array de usuários
+      if (alreadyExists.length===0) {
+        if (typeof name === "string" && name) {
+          db.collection("users").insertOne({
             name: name,
-            LastStatus: Date.now()
-        });
-        res.sendStatus(201)
-    } else {
-        res.sendStatus("402")
-    }
-
-})
+            LastStatus: Date.now(),
+          });
+          res.sendStatus(201);
+        } else {
+          res.sendStatus(402);
+        }
+      } else {
+        res.sendStatus(409);
+      }
+    });
+});
 
 app.get("/participants", (req, res) => {
-	// buscando usuários
-	db.collection("users").find().toArray().then(users => {
-		res.send(users); // array de usuários
-	});
+  // buscando usuários
+  db.collection("users")
+    .find()
+    .toArray()
+    .then((users) => {
+      res.send(users); // array de usuários
+    });
 });
-
 
 app.listen(5000);
