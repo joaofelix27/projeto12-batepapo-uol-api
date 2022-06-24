da819241fb0cb05675a360e3a262fd1300e1ea13
joaofelix27 <joaofelix27@gmail.com>
2022-06-24 12:02:26 -0300

    Versão post /participants OK

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import joi from "joi";
+import dayjs from "dayjs";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+const userSchema = joi.object({
+  name: joi.string().alphanum().required(),
+});
+
 const mongoClient = new MongoClient("mongodb://localhost:27017");
 let db;
-
 mongoClient.connect().then(() => {
   db = mongoClient.db("banco_de_dados_uol");
 });
-
-app.post("/participants", (req, res) => {
+app.post("/participants", async (req, res) => {
   let { name } = req.body;
-  db.collection("users")
-    .find({
-      name: name,
-    })
-    .toArray()
-    .then((alreadyExists) => {
-      console.log(alreadyExists); // users array
-      if (alreadyExists.length === 0) {
-        if (typeof name === "string" && name) {
-          db.collection("users").insertOne({
-            name: name,
-            LastStatus: Date.now(),
-          });
-          res.sendStatus(201);
-          db.collection("messages").insertOne({
-            from: name,
-            to: "Todos",
-            text: "entra na sala...",
-            type: "status",
-            time: "00:00:06",
-          });
-        } else {
-          res.sendStatus(402);
-        }
-      } else {
-        res.sendStatus(409);
-      }
-    });
+  const validation = userSchema.validate(req.body, { abortEarly: true });
+
+  if (validation.error) {
+    res.sendStatus(422);
+    return;
+  } 
+  try {
+    const alreadyExists = await db.collection("users").find({ name: name }) .toArray();
+    console.log(alreadyExists); // if the current name already exists, this variable.lenght is !0
+    if (alreadyExists.length === 0) {
+      let now=dayjs()
+     await db.collection("users").insertOne({
+       name: name,
+       LastStatus: Date.now(),
+     });
+    await db.collection("messages").insertOne({
+       from: name,
+       to: "Todos",
+       text: "entra na sala...",
+       type: "status",
+       time: now.format("HH:mm:ss"),
+     });
+     res.sendStatus(201);
+   } else {
+     res.sendStatus(409);
+   }
+   } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+   }
 });
 
 app.get("/participants", (req, res) => {
